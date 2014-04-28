5103632b709df16d08b750eed81ee2efb1a89c26
mn127 <mjn4406@gmail.com>
2014-04-28 12:56:13 -0400

    Add participant model

--- src/org/adaptlab/chpir/android/models/Participant.java
+++ src/org/adaptlab/chpir/android/models/Participant.java
@@ -1,5 +1,60 @@
 package org.adaptlab.chpir.android.models;
 
-public class Participant {
+import org.adaptlab.chpir.android.activerecordcloudsync.SendModel;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import android.util.Log;
+
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+@Table(name = "Participant")
+public class Participant extends SendModel {
+    private static final String TAG = "Participant";
+    
+    @Column(name = "SentToRemote")
+    private boolean mSent;
+    @Column(name = "ParticipantType")
+    private ParticipantType mParticipantType;
+    
+    public Participant(ParticipantType participantType) {
+        super();
+        mParticipantType = participantType;
+    }
+
+    @Override
+    public JSONObject toJSON() {
+        JSONObject json = new JSONObject();
+        
+        try {
+            JSONObject jsonObject = new JSONObject();
+            // TODO: Change to participant id
+            jsonObject.put("participant_type_id", getParticipantType().getRemoteId());
+            
+            json.put("participant", jsonObject);
+        } catch (JSONException je) {
+            Log.e(TAG, "JSON exception", je);
+        }
+        return json;
+    }
+    
+    public ParticipantType getParticipantType() {
+        return mParticipantType;
+    }
+
+    @Override
+    public boolean isSent() {
+        return mSent;
+    }
+
+    @Override
+    public boolean readyToSend() {
+        return true;
+    }
+
+    @Override
+    public void setAsSent() {
+        mSent = true;
+    }
 }
