639daa8bb456e27a30d6dac3d8d4c1beeb1c17a6
mn127 <mjn4406@gmail.com>
2014-04-28 11:02:43 -0400

    Add ParticipantType model

--- src/org/adaptlab/chpir/android/models/Property.java
+++ src/org/adaptlab/chpir/android/models/Property.java
@@ -1,5 +1,14 @@
 package org.adaptlab.chpir.android.models;
 
-public class Property {
+import org.adaptlab.chpir.android.activerecordcloudsync.ReceiveModel;
+import org.json.JSONObject;
+
+public class Property extends ReceiveModel {
+
+    @Override
+    public void createObjectFromJSON(JSONObject jsonObject) {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
