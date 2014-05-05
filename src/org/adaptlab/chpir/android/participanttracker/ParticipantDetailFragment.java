f9ad61c8d141cdad2c21d7d6d4856b0bb26b1a1e
mn127 <mjn4406@gmail.com>
2014-05-05 11:08:02 -0400

    Load participant properties in participant detail view

--- src/org/adaptlab/chpir/android/participanttracker/ParticipantDetailFragment.java
+++ src/org/adaptlab/chpir/android/participanttracker/ParticipantDetailFragment.java
@@ -1,10 +1,16 @@
 package org.adaptlab.chpir.android.participanttracker;
 
 import org.adaptlab.chpir.android.models.Participant;
+import org.adaptlab.chpir.android.models.ParticipantProperty;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ParticipantDetailFragment extends Fragment {
     private static final String TAG = "ParticipantDetailFragment";
@@ -12,6 +18,7 @@ public class ParticipantDetailFragment extends Fragment {
             "org.adaptlab.chpir.participanttracker.participantdetailfragment.participant_id";
     
     private Participant mParticipant;
+    private LinearLayout mParticipantPropertiesContainer;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,5 +33,22 @@ public class ParticipantDetailFragment extends Fragment {
             mParticipant = Participant.findById(participantId);
             Log.i(TAG, String.valueOf(mParticipant));
         }
+    }
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+            Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_participant_detail, parent,
+                false);       
+        
+        mParticipantPropertiesContainer = (LinearLayout) v.findViewById(R.id.participant_properties_container);
+        
+        for (ParticipantProperty participantProperty : mParticipant.getParticipantProperties()) {
+            TextView textView = new TextView(getActivity());
+            textView.setText(participantProperty.getProperty().getLabel() + ": " + participantProperty.getValue());
+            mParticipantPropertiesContainer.addView(textView);
+        }
+        
+        return v;
     }
 }
