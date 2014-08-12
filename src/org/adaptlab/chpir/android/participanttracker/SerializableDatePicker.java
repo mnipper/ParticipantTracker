304d037a9638d9c07609097f6dcce3828714d18a
mn127 <mjn4406@gmail.com>
2014-08-12 11:41:53 -0400

    Add Serializeable DatePicker

---
+++ src/org/adaptlab/chpir/android/participanttracker/SerializableDatePicker.java
@@ -0,0 +1,29 @@
+package org.adaptlab.chpir.android.participanttracker;
+
+import android.content.Context;
+import android.widget.DatePicker;
+
+// Serialize and deserialize a DatePicker in the format Month-Day-Year
+
+public class SerializableDatePicker extends DatePicker {
+
+    public SerializableDatePicker(Context context) {
+        super(context);
+    }
+
+    public String serialize() {
+        return this.getMonth() + "-" + this.getDayOfMonth() + "-" + this.getYear();
+    }
+    
+    public void deserialize(String serializedDate) {
+        if (serializedDate.equals("")) return;
+        String[] dateComponents = serializedDate.split("-");
+        int month, day, year;
+        if (dateComponents.length == 3) {
+            month = Integer.parseInt(dateComponents[0]) - 1;
+            day = Integer.parseInt(dateComponents[1]);
+            year = Integer.parseInt(dateComponents[2]);
+            this.updateDate(year, month, day);
+        }
+    }
+}
